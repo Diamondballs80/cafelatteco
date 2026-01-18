55a95df37c21e1742b6f7438bcf2e1d326da04ba
Wes Hager <weshager@macbookpro.attlocal.net>
2026-01-17 20:18:12 -0600

    Improve hero video fade animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,7 +152,7 @@ const renderHero = (site) => {
     const delayedShow = () => requestAnimationFrame(() => requestAnimationFrame(show));
     iframe.addEventListener("load", delayedShow);
     // Fallback in case load fires before listener is attached or is cached
-    setTimeout(show, 800);
+    setTimeout(show, 1500);
   }
 };
 
