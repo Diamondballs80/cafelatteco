90e3466979e18c227663a246a9c9b580d97e31a1
Wes Hager <weshager@macbookpro.attlocal.net>
2026-01-17 20:25:21 -0600

    Fade video on wrapper to ensure visible animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,11 +147,13 @@ const renderHero = (site) => {
   `;
 
   const iframe = hero.querySelector(".hero__video iframe");
+  const videoWrapper = hero.querySelector(".hero__video");
   if (iframe) {
     const show = () => {
+      if (!videoWrapper) return;
       // Force reflow so opacity transition reliably plays
-      void iframe.offsetWidth;
-      iframe.classList.add("is-visible");
+      void videoWrapper.offsetWidth;
+      videoWrapper.classList.add("is-visible");
     };
     const delayedShow = () => requestAnimationFrame(() => requestAnimationFrame(show));
     iframe.addEventListener("load", delayedShow);
